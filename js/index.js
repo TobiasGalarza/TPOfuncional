14de213247ff33975686ea55f8e9236d15d3a18e
tobias galarza <77801758+TobiasGalarza@users.noreply.github.com>
2021-07-11 05:43:47 -0300

    1er commit TPOfuncional

---
+++ js/index.js
@@ -0,0 +1,23 @@
+header = `
+<h1><a href="index.html">Portal de Oficios</a></h1>
+<nav>
+	<a href="index.html">inicio</a>
+	<a href="search.html">búsqueda</a>
+	<a href="join.html">únete</a>
+	<a href="login.html">iniciá sesión</a>
+</nav>
+`;
+document.getElementById("header").innerHTML = header;
+
+
+footer = `
+<p><b>&#169todos los derechos reservados</b></p>
+<div>
+	<a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+	<a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+	<a href="https://www.github.com" target="_blank"><i class="fab fa-github"></i></a>
+	<a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+	<a href="https://web.whatsapp.com" target="_blank"><i class="fab fa-whatsapp"></i></a>
+</div>
+`;
+document.getElementById("footer").innerHTML = footer;
